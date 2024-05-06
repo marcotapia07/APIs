b7947f644f74859e2c4b1027660810ae407738cb
MarcT <133397932+marcotapia07@users.noreply.github.com>
2024-05-05 22:44:02 -0500

    Add files via upload

---
+++ datosPokemon.js
@@ -0,0 +1,26 @@
+const consultarPokemon = (id, number) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            pintarPokemon(data, number);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+const btnSeleccionar = () => {
+    let primerPokemon = Math.round(Math.random() * 150);
+    let segundoPokemon = Math.round(Math.random() * 150);
+    consultarPokemon(primerPokemon, 1);
+    consultarPokemon(segundoPokemon, 2);
+};
+const lista = document.getElementById("listarpokemon")
+const pintarPokemon = (data, id) => {
+    let item = lista.querySelector(`#pok-${id}`);
+    item.getElementsByTagName("img")[0].setAttribute("src", data.sprites.front_shiny);
+    item.getElementsByTagName("p")[0].innerHTML = data.name;
+}
